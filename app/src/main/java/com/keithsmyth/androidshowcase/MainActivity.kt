9f5d304e87fde60f04efb87c83efab8e8ec94f18
Keith Smyth <smyth.keith@gmail.com>
2024-02-12 19:47:15 -0500

    initial project creation

---
+++ app/src/main/java/com/keithsmyth/androidshowcase/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.keithsmyth.androidshowcase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
