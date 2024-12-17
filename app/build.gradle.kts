c320aa4b4a9fe021086e8ad95b3a32c536b2bc7e
Keith Smyth <smyth.keith@gmail.com>
2024-12-16 21:01:34 -0500

    Updoot versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.keithsmyth.androidshowcase"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.keithsmyth.androidshowcase"
         minSdk = 28
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
