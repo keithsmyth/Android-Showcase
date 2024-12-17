d35d22c7c2032de6188340280e2e863844c67671
Keith Smyth <smyth.keith@gmail.com>
2024-12-16 21:02:40 -0500

    Singleton

--- app/src/main/java/com/keithsmyth/androidshowcase/ShowcaseModule.kt
+++ app/src/main/java/com/keithsmyth/androidshowcase/ShowcaseModule.kt
@@ -5,12 +5,14 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import kotlinx.serialization.json.Json
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object ShowcaseModule {
 
     @Provides
+    @Singleton
     fun provideJson(): Json {
         return Json {
             ignoreUnknownKeys = true
